64e71ce4ee8ed11055ef005660d1848ce7324110
Jakub Szafraniec <>
2023-01-23 09:01:45 +0100

    web-types added

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() disabled: boolean;
+  @Input() style: string;
+  @Input() size: string;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
